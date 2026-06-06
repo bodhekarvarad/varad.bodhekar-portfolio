3345025bb3dce7ab0e0445d2d6f2ae85d8bc6520
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-06 11:40:27 +0530

    created footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 const Footer=()=>{
 return(
-    <div>
-        <h1>
-            Footer
-        </h1>
+    <div className=" bg-black">
+        
+          <p className="text-white text-center"><i class="fa-regular fa-copyright"></i><b> 2026  Varad Bodhekar  All Right Reserved.</b> </p>
+              
     </div>
 )
 }
