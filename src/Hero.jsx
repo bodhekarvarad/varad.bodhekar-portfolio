ae727a230743a47085617e1e80c8331d72c653db
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-07 20:25:19 +0530

    add card

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,22 +3,63 @@ import Navbar from "./Navbar";
 import "./Style.css"
 const Hero=()=>{
     return(
-        <div className="hero-bg">
-          <div className="col-6">
-          <div className="row" id="hero-name">
-            <h3 className="text-white mt-3"><b>Hi,I'm</b></h3>
-            <h1 className="text-white mt-2"> <b>Varad</b> <span className="name"><b>Bodhekar</b></span></h1>
-            <h4 className="text-primary text-wrap p-1 mt-1">AI/ML Enthusiast | MERN Stack | Android </h4>
-            <p className="f-1 text-white mt-1"><b>B.E student passinate about building intelligent solution using Machine and Web Technologies</b></p>
-            <div className="col">
-     <button className="btn btn-primary btn-lg mx-1">View Projects</button>
-     <button className="btn btn-outline-light mx-3"> Download Resume <i class="fa-solid fa-download"></i></button>
-           <button className="btn btn-outline-light mx-1"><i class="fa-regular fa-envelope"></i> Contact Me</button>
-            </div>
-          </div>
-         
-        </div>
-        </div>
+       <div className="row hero-bg">
+  <div className="col-6">
+    <div id="hero-name">
+      <h3 className="text-white mt-3"><b>Hi, I'm</b></h3>
+      <h1 className="text-white mt-2">
+        <b>Varad</b> <span className="name"><b>Bodhekar</b></span>
+      </h1>
+      <h4 className="text-primary text-wrap p-1 mt-1">
+        AI/ML Enthusiast | MERN Stack | Android
+      </h4>
+      <p className="text-white mt-1">
+        <b>
+          B.E student passionate about building intelligent solutions using
+          Machine Learning and Web Technologies
+        </b>
+      </p>
+
+      <button className="btn btn-primary btn-lg mx-1">
+        View Projects
+      </button>
+      <button className="btn btn-outline-light mx-3">
+        Download Resume <i className="fa-solid fa-download"></i>
+      </button>
+      <button className="btn btn-outline-light mx-1">
+        <i className="fa-regular fa-envelope"></i> Contact Me
+      </button>
+    </div>
+  </div>
+<div className="col-6 d-flex justify-content-center align-items-center">
+  <div
+    className="card  what-i-do-card text-white border-0"
+    
+  >
+    <div className="card-body p-4">
+      <h4 className="card-title mb-4">What I Do</h4>
+
+      <p className="card-text mb-3">
+        <i className="fa-solid fa-robot me-3"></i>
+        Build Machine Learning Models
+      </p>
+
+      <p className="card-text mb-3">
+        <i className="fa-solid fa-code me-3"></i>
+        Develop Full Stack Web Applications
+      </p>
+
+      <p className="card-text">
+        <i className="fa-solid fa-globe me-3"></i>
+        Solve Real-world Problems
+      </p>
+    </div>
+  </div>
+</div>
+
+</div>
+           
+        
     );
 }
 export default Hero;
