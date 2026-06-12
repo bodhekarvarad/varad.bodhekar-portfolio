f007d2a4744eb1b45f7691739f277bd68d434b57
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-12 18:03:48 +0530

    add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 const Skills=()=>{
     return(
-<div>
-    <h1>
-        Skills
-    </h1>
+<div className="row mt-2">
+    <h1 className="text-center skills f-5"><b>Skills</b></h1>
+   <div className="col-4">
+      <div className="card text-black border-0 mt-5 text-center">
+      <h3 className="card-text mb-3 mx-3  ">
+       <i class="fa-solid fa-code text-primary"></i>{" "}<span>Languages</span>
+      </h3>
+     <div>
+        <ul>
+            <li>
+            Python
+        </li>
+         <li>
+            Java
+        </li>
+         <li>
+            JavaScript
+        </li>
+         <li>
+            SQL
+        </li>
+        </ul>
+     
+      </div>
+    
+      </div>
+   </div>
 </div>
     )
 }
