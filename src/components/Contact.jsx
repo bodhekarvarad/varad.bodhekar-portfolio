068f06f1f35d894990b39d078271bf98153df35b
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-21 17:35:06 +0530

    add a new div

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,13 @@ import React from "react";
 const Contact=()=>{
     return(
         <div>
-            <h1>
-                Contact
-            </h1>
+           <div className="row">
+            <div className="mb-3 mt-2" style={{
+                marginLeft:"5rem"
+            }}>
+                <i class="fa-regular fa-paper-plane fs-1"></i> <span className="fs-2 "><b>Contact Me</b></span>
+            </div>
+           </div>
         </div>
     )
 }
