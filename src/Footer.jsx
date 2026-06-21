1e007e108acf987db1efc43a984501b3470f32ea
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-21 17:34:03 +0530

    add margin top

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 const Footer=()=>{
 return(
-    <div className=" bg-black">
-        
-          <p className="text-white text-center"><i class="fa-regular fa-copyright"></i><b> 2026  Varad Bodhekar  All Right Reserved.</b> </p>
-              
-    </div>
+     <div className="bg-black">
+        <div>
+            <p className="text-white text-center mb-0"><i class="fa-regular fa-copyright"></i><b> 2026  Varad Bodhekar  All Right Reserved.</b> </p>    
+        </div>
+          
+ </div>
 )
 }
 export default Footer;
