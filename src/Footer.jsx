80a2ef938f29d6cecb2a55d8a03ab80775ba3ff7
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-07-03 19:15:44 +0530

    add for footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-
 const Footer = () => {
       const year = new Date().getFullYear();
   return (
-   
     <footer className="bg-black py-3">
       <p className="text-white text-center mb-0">
         <i className="fa-regular fa-copyright"></i>
